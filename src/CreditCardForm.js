5386c1f3c9fcdeb55eaf4ac02fbb59b0ec7ba7d4
Helen Quexel <helenzzzzquexel@gmail.com>
2023-09-08 11:29:10 -0600

    Caambios al input

--- src/CreditCardForm.js
+++ src/CreditCardForm.js
@@ -1,42 +1,61 @@
 // src/CreditCardForm.js
 
-
 import React, { useState } from 'react';
 import Cards from 'react-credit-cards-2';
 import 'react-credit-cards-2/dist/es/styles-compiled.css';
 
-
-
 function CreditCardForm() {
-
-const [state, setState]= useState({
+  const [state, setState] = useState({
     number: '',
     name: '',
     expiry: '',
     cvc: '',
     focus: '',
+  });
 
-});
+  const [errors, setErrors] = useState({
+    number: '',
+    name: '',
+    expiry: '',
+    cvc: '',
+  });
 
-const handledata = (e) =>{
+  const handledata = (e) => {
     setState({
-        ...state,
-        [e.target.name]: e.target.value
+      ...state,
+      [e.target.name]: e.target.value,
     });
-}
+  };
 
-
-const handlenfoque = (e) =>{
+  const handlenfoque = (e) => {
     setState({
-        ...state,
-      focus: e.target.name
+      ...state,
+      focus: e.target.name,
     });
-}
-  
-  return   (
-      
-      <div>
-        <Cards
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validación de campos requeridos
+    const newErrors = {
+      number: state.number === '' ? 'El número de tarjeta es obligatorio' : '',
+      name: state.name === '' ? 'El nombre en la tarjeta es obligatorio' : '',
+      expiry: state.expiry === '' ? 'La fecha de vencimiento es obligatoria' : '',
+      cvc: state.cvc === '' ? 'El CVC es obligatorio' : '',
+    };
+
+    setErrors(newErrors);
+
+    // Si no hay errores, puedes enviar el formulario o realizar otras acciones.
+    if (!Object.values(newErrors).some((error) => error !== '')) {
+      // Envía el formulario o realiza otras acciones aquí.
+    }
+  };
+
+  return (
+    <div>
+      <Cards
         number={state.number}
         expiry={state.expiry}
         name={state.name}
@@ -44,63 +63,66 @@ const handlenfoque = (e) =>{
         focused={state.focus}
       />
 
-    <div className="credit-card-form">
-      <h4 >Ingrese los datos de su tarjeta de crédito</h4>
-      <form>
-        <div>
-          <label>Número de tarjeta:</label>
-          <input 
-            className='cajas'
-            type="number"
-            onFocus={handlenfoque}
-            name="number"
-            id="number"
-            onChange={handledata}
-            placeholder="Numero de tarjeta"
-          />
-        </div>
-        <div>
-          <label>Nombre en la tarjeta:</label>
-          <input
-            className='cajas'
-            type="name"
-            onFocus={handlenfoque}
-            name="name"
-            id="name"
-            onChange={handledata}
-            placeholder="Ingrese su nombre"
-          />
-        </div>
-        <div>
-          <label>Fecha de vencimiento:</label>
-          <input
-            type="number"
-            className='cajas'
-            onFocus={handlenfoque}
-            name="expiry"
-            id="expiry"
-            onChange={handledata}
-           placeholder="MM/YY"
-          />
-        </div>
-        <div>
-          <label>CVC:</label>
-          <input
-            type="number"
-            className='cajas'
-            onFocus={handlenfoque}
-            name="cvc"
-            id="cvc"
-            onChange={handledata}
-            placeholder="ingrese CVC"
-          />
-        </div>
-        
-        <div className='space'>
-        <button className='btn'>pagar</button>
-        </div>
-      </form>
-    </div>
+      <div className="credit-card-form">
+        <h4>Ingrese los datos de su tarjeta de crédito</h4>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Número de tarjeta:</label>
+            <input
+              className='cajas'
+              type="number"
+              onFocus={handlenfoque}
+              name="number"
+              id="number"
+              onChange={handledata}
+              placeholder="Numero de tarjeta"
+            />
+            <p className="error">{errors.number}</p>
+          </div>
+          <div>
+            <label>Nombre en la tarjeta:</label>
+            <input
+              className='cajas'
+              type="text"
+              onFocus={handlenfoque}
+              name="name"
+              id="name"
+              onChange={handledata}
+              placeholder="Ingrese su nombre"
+            />
+            <p className="error">{errors.name}</p>
+          </div>
+          <div>
+            <label>Fecha de vencimiento:</label>
+            <input
+              type="number"
+              className='cajas'
+              onFocus={handlenfoque}
+              name="expiry"
+              id="expiry"
+              onChange={handledata}
+              placeholder="MM/YY"
+            />
+            <p className="error">{errors.expiry}</p>
+          </div>
+          <div>
+            <label>CVC:</label>
+            <input
+              type="number"
+              className='cajas'
+              onFocus={handlenfoque}
+              name="cvc"
+              id="cvc"
+              onChange={handledata}
+              placeholder="Ingrese CVC"
+            />
+            <p className="error">{errors.cvc}</p>
+          </div>
+          <div className='space'>
+            <button className='btn' type="submit">Pagar</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
